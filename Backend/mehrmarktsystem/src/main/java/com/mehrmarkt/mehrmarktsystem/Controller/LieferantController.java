e9db889cf4d1211af6ce7a93ca43a21859bd14a6
majd bousaad <bousaadmajd@gmail.com>
2022-10-23 23:57:30 +0200

    changing path to /lieferrant insteadof /Lieferant

--- Backend/mehrmarktsystem/src/main/java/com/mehrmarkt/mehrmarktsystem/Controller/LieferantController.java
+++ Backend/mehrmarktsystem/src/main/java/com/mehrmarkt/mehrmarktsystem/Controller/LieferantController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("/Lieferant")
+@RequestMapping("/lieferant")
 public class LieferantController {
 
     @Autowired
