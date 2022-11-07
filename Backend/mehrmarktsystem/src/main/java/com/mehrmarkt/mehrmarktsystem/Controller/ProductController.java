86ba7e91bb6d37b12311725dfa4ef2c66612d507
majd bousaad <bousaadmajd@gmail.com>
2022-11-07 17:59:49 +0100

    Adding /product/assignToLiefeant/{lieferantid} API

--- Backend/mehrmarktsystem/src/main/java/com/mehrmarkt/mehrmarktsystem/Controller/ProductController.java
+++ Backend/mehrmarktsystem/src/main/java/com/mehrmarkt/mehrmarktsystem/Controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.mehrmarkt.mehrmarktsystem.Controller;
 
 
+import com.mehrmarkt.mehrmarktsystem.Service.LieferantService;
 import com.mehrmarkt.mehrmarktsystem.Service.ProductService;
+import com.mehrmarkt.mehrmarktsystem.model.Lieferant;
 import com.mehrmarkt.mehrmarktsystem.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +18,9 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private LieferantService lieferantService;
+
     @PostMapping("/add")
     public String add(@RequestBody Product product){
         productService.saveProduct(product);
@@ -25,6 +31,17 @@ public class ProductController {
     @GetMapping("/all")
     public List<Product> getAll(){
         return productService.getAllProducts();
+    }
+
+    @PostMapping("/assignToLieferant/{lieferant_id}")
+    public String assignToLieferant(@RequestBody List<Product> products, @PathVariable int lieferant_id){
+
+        Lieferant lieferant = lieferantService.getById(lieferant_id);
+        lieferant.getProducts().addAll(products);
+
+        lieferantService.saveLieferant(lieferant);
+
+        return "Adding Product " + products.get(0).getName() + " to Lieferant " + lieferant.getName();
     }
 
 
