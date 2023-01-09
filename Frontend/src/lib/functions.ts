e8aee7aaa9e524b5252be750ff466f229ff9a28a
Kacper Cömlek <kacper.coemlek@kanzleipilot.de>
2023-01-09 16:35:28 +0100

    Lösche Lieferant (1/2)

--- Frontend/src/lib/functions.ts
+++ Frontend/src/lib/functions.ts
@@ -8,19 +8,9 @@ function padTo2Digits(num: number) {
   // You can tweak formatting easily
 export function formatDate(date: Date) {
     return (
-      
-      [
-        
-        padTo2Digits(date.getDate()),
-        padTo2Digits(date.getMonth() + 1),
-        date.getFullYear()
-      ].join('.') +
+      [padTo2Digits(date.getDate()), padTo2Digits(date.getMonth() + 1), date.getFullYear()].join('.') +
       ' um ' +
-      [
-        padTo2Digits(date.getHours()),
-        padTo2Digits(date.getMinutes()),
-        padTo2Digits(date.getSeconds()),
-      ].join(':')
-      + ' Uhr'
-    );
+      [padTo2Digits(date.getHours()), padTo2Digits(date.getMinutes()), padTo2Digits(date.getSeconds())].join(':') +
+      ' Uhr'
+    )
   }
